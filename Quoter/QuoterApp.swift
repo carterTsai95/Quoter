c56822c6a3819b3c9586a55728a85c06c6a8a2f6
Hung-Chun, Tsai <tsaihong1995@gmail.com>
2021-04-16 22:12:28 -0400

    Initial Commit

---
+++ Quoter/QuoterApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuoterApp.swift
+//  Quoter
+//
+//  Created by Hung-Chun Tsai on 2021-04-16.
+//
+
+import SwiftUI
+
+@main
+struct QuoterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
